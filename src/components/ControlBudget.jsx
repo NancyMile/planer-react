6ad5b5d953d3abf3d25b6b9c41d4a42336003879
nancy <smithnancy@outlook.com>
2023-08-28 19:11:03 +1000

    format numbers to currency

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -1,11 +1,27 @@
 const ControlBudget = ({ budget }) => {
+
+    const formatAmount = (amount) => {
+        return amount.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
+
   return (
       <div className="contenedor-presupuesto contenedor sombra dos-columnas">
           <div>
               <p>Graphic</p>
               <div className="contenido-presupuesto">
                   <p>
-                      <span>Budget:</span> ${budget}
+                      <span>Budget:</span>{formatAmount(budget)}
+                  </p>
+
+                  <p>
+                      <span>Available:</span>{formatAmount(0)}
+                  </p>
+
+                  <p>
+                      <span>Spent:</span>{formatAmount(0)}
                   </p>
               </div>  
           </div>
