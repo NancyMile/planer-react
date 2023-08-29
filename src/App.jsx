18712e001b236e59c1d25303e4e66555d7003677
nancy <smithnancy@outlook.com>
2023-08-29 12:15:13 +1000

    Display and hide modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from './components/Header'
 import { useState } from 'react'
 import newExpense from './img/nuevo-gasto.svg';
+import Modal from './components/Modal';
 
 function App() {
 
   const [budget, setBudget] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const handleNewSpent = () => {
+    setModal(true);
+  }
 
   return (
     <div>
@@ -18,9 +24,15 @@ function App() {
 
       {isValidBudget && (
           <div className='nuevo-gasto'>
-            <img src={newExpense} alt="new expense" />
+            <img src={newExpense} alt="new expense" onClick={handleNewSpent} />
           </div>
         )
+      }
+
+      {modal &&
+        <Modal
+          setModal={setModal}
+        />
       }
 
     </div>
